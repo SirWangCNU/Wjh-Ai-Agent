36d054057200f6c6c3a81844d037660d01d6f558
SirWangCNU <2241002095@cnu.edu.cn>
2026-03-12 20:23:44 +0800

    Resolve merge conflicts

--- src/test/java/com/wjh/wjhaiagent/WjhAiAgentApplicationTests.java
+++ src/test/java/com/wjh/wjhaiagent/WjhAiAgentApplicationTests.java
@@ -1,27 +1,25 @@
 package com.wjh.wjhaiagent;
 
-<<<<<<< HEAD
+
 import com.wjh.wjhaiagent.rag.LoveAppDocumentLoader;
 import jakarta.annotation.Resource;
-=======
->>>>>>> 1a2d6159bb5b98bed9949eab30b939cef64223c1
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WjhAiAgentApplicationTests {
-<<<<<<< HEAD
+
     @Resource
     private LoveAppDocumentLoader loveAppDocumentLoader;
 
     @Test
     void contextLoads() {
         loveAppDocumentLoader.loadMarkdowns();
-=======
 
     @Test
     void contextLoads() {
->>>>>>> 1a2d6159bb5b98bed9949eab30b939cef64223c1
+
     }
 
 }
