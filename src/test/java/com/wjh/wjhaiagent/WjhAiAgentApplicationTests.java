1a2d6159bb5b98bed9949eab30b939cef64223c1
SirWangCNU <2241002095@cnu.edu.cn>
2026-03-12 13:03:27 +0800

    恋爱应用第一版

---
+++ src/test/java/com/wjh/wjhaiagent/WjhAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjh.wjhaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjhAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
